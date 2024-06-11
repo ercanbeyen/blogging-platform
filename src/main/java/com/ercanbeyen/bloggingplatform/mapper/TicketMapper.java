ed7bfa7d63f619a703ab02a741b4eae72a265cdf
Ercan Beyen <96631352+ercanbeyen@users.noreply.github.com>
2024-06-12 01:30:37 +0300

    Add ResultMap into ticket mapper

--- src/main/java/com/ercanbeyen/bloggingplatform/mapper/TicketMapper.java
+++ src/main/java/com/ercanbeyen/bloggingplatform/mapper/TicketMapper.java
@@ -7,36 +7,43 @@ import java.util.List;
 
 @Mapper
 public interface TicketMapper {
+
     @Insert("""
-            INSERT INTO tickets(description)
+            INSERT INTO TICKETS (DESCRIPTION)
             VALUES (#{description})
             """)
+    @Options(useGeneratedKeys = true, keyProperty = "id")
     void insertTicket(Ticket ticket);
 
     @Update("""
-            UPDATE tickets
-            SET description = #{ticket.description}
-            WHERE id = #{id}
+            UPDATE TICKETS
+            SET DESCRIPTION = #{ticket.description}
+            WHERE ID = #{id}
             """)
     void updateTicket(@Param("id") Integer id, @Param("ticket") Ticket ticket);
 
+    @Results(id = "ticketResultMap", value = {
+            @Result(property = "id", column = "ID"),
+            @Result(property = "description", column = "DESCRIPTION")
+    })
     @Select("""
             SELECT *
-            FROM tickets
-            WHERE id = #{id}
+            FROM TICKETS
+            WHERE ID = #{id}
             """)
     Ticket findTicket(@Param("id") Integer id);
 
+    @ResultMap("ticketResultMap")
     @Select("""
             SELECT *
-            FROM tickets
+            FROM TICKETS
             """)
     List<Ticket> findTickets();
 
     @Delete("""
             DELETE
-            FROM tickets
-            WHERE id = #{id}
+            FROM TICKETS
+            WHERE ID = #{id}
             """)
     void deleteTicket(@Param("id") Integer id);
 }
--- src/test/java/com/ercanbeyen/bloggingplatform/BloggingPlatformApplicationTests.java
+++ src/test/java/com/ercanbeyen/bloggingplatform/BloggingPlatformApplicationTests.java
@@ -3,7 +3,7 @@ package com.ercanbeyen.bloggingplatform;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = BloggingPlatformApplicationTests.class)
+@SpringBootTest
 class BloggingPlatformApplicationTests {
 
 	@Test
