3ca62cb1e17293049135f5ad26c4e478b72d617a
Ercan Beyen <96631352+ercanbeyen@users.noreply.github.com>
2024-07-18 15:56:17 +0300

    Refactor validators

--- src/main/java/com/ercanbeyen/bloggingplatform/validator/PasswordValidator.java
+++ src/main/java/com/ercanbeyen/bloggingplatform/validator/PasswordValidator.java
@@ -16,14 +16,14 @@ public class PasswordValidator implements ConstraintValidator<PasswordRequest, S
 
     @Override
     public boolean isValid(String password, ConstraintValidatorContext constraintValidatorContext) {
-        String regex =  "^" +                   // start-of-string
-                        "(?=.*[0-9])" +         // a digit must occur at least once
+        String regex =  "^" +                   // start of string
+                        "(?=.*\\d)" +           // a digit must occur at least once
                         "(?=.*[a-z])" +         // a lower case letter must occur at least once
                         "(?=.*[A-Z])" +         // an upper case letter must occur at least once
                         "(?=.*[@#$%^&+=])" +    // a special character must occur at least once
                         "(?=\\S+$)" +           // no whitespace allowed in the entire string
                         ".{8,}" +               // anything, at least eight places though
-                        "$";                    // end-of-string
+                        "$";                    // end of string
 
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(password);
